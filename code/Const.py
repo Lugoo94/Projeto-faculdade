ff3884662daa64a6cff235ff40b721e5fa2980df
Luis Gustavo <luisgustavo.mac13@gmail.com>
2025-06-04 21:30:02 -0300

    Criando pasta de assets Inserindo assets Criando menu: background, som e fontes Criando o arquivo para constantes

---
+++ code/Const.py
@@ -0,0 +1,14 @@
+# Constantes que começam com "c"
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# Constantes que começam com "m"
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# Constantes que começam com "w"
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/Menu.py
+++ code/Menu.py
@@ -1,6 +1,40 @@
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+
+
 class Menu:
     def __init__(self, window):
         self.window = window
+        # carregar a imagem selecionada
+        self.surf = pygame.image.load('./asset/MenuBg.png')  # "surf" é o nome criado para a variável
+        self.rect = self.surf.get_rect(left=0, top=0)  # get_rect() serve para dizer onde começa o retângulo criado
+        # "left=0" e "top=0" já é a posição padrão de onde começa o retângulo
 
     def run(self):
-        pass
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)  # 0 "(-1)" serve para a música ficar em loop
+
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))  # MIN_WIDTH / 2 para o texto ficar no centro da tela e altura de 70
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
+            pygame.display.flip()
+
+            # Checar por todos os eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # fecha a janela
+                    quit()  # fecha o pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
