c415e7aafca3a83b0aa037222aa891ee89c69315
Luis Gustavo <luisgustavo.mac13@gmail.com>
2025-06-10 21:00:57 -0300

    Criando jogadores, inimigos e seus movimentos

--- code/Const.py
+++ code/Const.py
@@ -1,9 +1,12 @@
-# Constantes que começam com "c"
+# Constantes que começam com "C"
+import pygame
+
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW =(255, 255, 128)
 
-# Constantes que começam com "e"
+# Constantes que começam com "E"
+EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -21,19 +24,34 @@ ENTITY_SPEED = {
     'Player1Shot': 1,
     'Player2': 3,
     'Player2Shot': 3,
-    'Enemy1': 1,
+    'Enemy1': 2,
     'Enemy1Shot': 5,
     'Enemy2': 1,
     'Enemy2Shot': 2,
 }
 
-# Constantes que começam com "m"
+# Constantes que começam com "M"
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
 
-# Constantes que começam com "w"
+# Constantes que começam com "P"
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
+
+# Constantes que começam com "S"
+SPAWN_TIME = 4000  # 4 segundos
+
+# Constantes que começam com "W"
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
---
+++ code/Player.py
@@ -0,0 +1,22 @@
+import pygame.key
+
+from code.Const import WIN_HEIGHT, WIN_WIDTH, ENTITY_SPEED, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, \
+    PLAYER_KEY_RIGHT
+from code.Entity import Entity
+
+class Player(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+
+    def move(self):
+        pressed_key = pygame.key.get_pressed()
+
+        if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0:  # quando bater no topo, para de subir
+            self.rect.centery -= ENTITY_SPEED[self.name]
+        if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT:  # se fosse feito com "elif", o jogo não reconheceria duas teclas sendo pressionadas ao mesmo tempo
+            self.rect.centery += ENTITY_SPEED[self.name]
+        if pressed_key[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+        if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH:
+            self.rect.centerx += ENTITY_SPEED[self.name]
+
